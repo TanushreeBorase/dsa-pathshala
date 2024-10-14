1062fe6c38e295cafb7dc88a6306edece4c41ded
Tanushree <130696335+TanushreeBorase@users.noreply.github.com>
2024-10-14 14:00:55 +0530

    Add files via upload

---
+++ Learn from scratch/datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 3;
+    cout << x << endl;
+    float y = 3.5;
+    cout << y << endl;
+    char z = 'a';
+    cout << z << endl;
+    double p = 6.456;
+    cout << p << endl;
+    bool m = true;
+    cout << m << endl;
+    const double PI = 3.14;
+    cout << PI << endl;
+    return 0;
+}
