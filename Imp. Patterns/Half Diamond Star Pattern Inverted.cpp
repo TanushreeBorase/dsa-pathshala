1e5bfab4675d0afd14e0294a7b2120e5cdee9d17
Tanushree <130696335+TanushreeBorase@users.noreply.github.com>
2024-10-15 17:12:50 +0530

    Create Half Diamond Star Pattern Inverted.cpp

---
+++ Imp. Patterns/Half Diamond Star Pattern Inverted.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter the Number: ";
+    cin >> num;
+
+    // First part: Upper pyramid
+    for (int i = 0; i < num; i++) {
+        // Print leading spaces
+        for (int j = 0; j < num - i - 1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // Second part: Inverted pyramid
+    for (int i = 1; i < num; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < num - i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
