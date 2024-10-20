7c81daf0849456deef9169e87a7fa01f6694c35f
Tanushree <130696335+TanushreeBorase@users.noreply.github.com>
2024-10-20 13:59:52 +0530

    Add files via upload

---
+++ Learn from scratch/switch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n =5;
+
+    switch(n){
+
+        case 1: cout << "first" << endl;
+                break;
+
+        case 2: cout << "second" << endl;
+                break;
+
+        case 3: cout << "third" << endl;
+                break;
+
+        default: cout << "default case aagaya" << endl;
+
+        
+    }
+
+    return 0;
+}
