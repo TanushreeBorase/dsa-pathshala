779e3706bf715abcec152067fe2bdbd273f5c62a
Tanushree <130696335+TanushreeBorase@users.noreply.github.com>
2024-11-17 13:43:27 +0530

    Create acess_element.cpp

---
+++ BabbarCode/C++ STL/vectors/acess_element.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<char> v = {'a', 'c', 'f', 'd', 'z'};
+
+    // Accessing and printing values using indexes
+      cout << v[3] << endl;
+   // vector at() for safely accessing elements
+      cout << v.at(2);
+  
+    return 0;
+}
