ae0ca4372c73880becec04ed1259f73e3443e082
Tanushree <130696335+TanushreeBorase@users.noreply.github.com>
2024-11-18 23:44:16 +0530

    Update binarysearch.cpp

--- BabbarCode/Binary search Practice/binarysearch.cpp
+++ BabbarCode/Binary search Practice/binarysearch.cpp
@@ -1,54 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-int binary_search(int array[],int low,int high,int element){
-    if(high > low){
-        int x = low + (high - low) / 2;
-        if(array[x] == element){
-            return x;
+int main() {
+    vector<int> arr = {2, 3, 4, 10, 40};
+    int target;
+
+    cout << "Enter the target element to search for: ";
+    cin >> target;
+
+    int left = 0;
+    int right = arr.size() - 1;
+    int foundIndex = -1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            foundIndex = mid;
+            break;
         }
-        else if(array[x]< element){
-            return binary_search(array,x+1,high,element);
-        }
-        else if(array[x]>element){
-            return binary_search(array,x+1,high,element);
-        }
-    }
-    return -1;
-}
 
-int main(){
-    int n;
-    cout << "Enter the number of elements: " << endl;
-    cin >> n;
-
-    int a[n];
-    cout << "The array elements are: " << endl;
-    for(int i=0;i<n;i++){
-        cin >> a[i];
+        if (arr[mid] < target)
+            left = mid + 1;
+         else
+            right = mid - 1;
     }
 
-    cout << "Array is: " << endl;
-    for(int i=0;i<n;i++){
-        cout << a[i] << endl;
-    }
-
-    int l=0;
-    int h = n-1;
-    int f;
-
-    cout << "Enter the element to be found: " << endl;
-    cin >> f;
-
-    int b = binary_search(a,l,h,f);
-    if(b == -1){
-        cout << "The element is not found "<<endl;
-    }
-    else{
-        cout << "The element is found at index " << b << endl;
-    }
-
+    if (foundIndex != -1)
+        cout << "Element found at index " << foundIndex << endl;
+    else
+        cout << "Element not found in the array" << endl;
 
     return 0;
-
 }
