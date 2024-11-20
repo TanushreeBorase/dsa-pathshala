e8a151088a8509feb3642627b1210c4106a51848
Tanushree <130696335+TanushreeBorase@users.noreply.github.com>
2024-11-20 12:19:04 +0530

    Create floor_ceil_problem.cpp

---
+++ BabbarCode/Binary search Practice/floor_ceil_problem.cpp
@@ -0,0 +1,40 @@
+int findFloor(vector<int> &arr, int n, int x) {
+	int low = 0, high = n - 1;
+	int ans = -1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+
+		if (arr[mid] <= x) {
+			ans = arr[mid];
+			low = mid + 1;
+		}
+		else {
+			high = mid - 1; 
+		}
+	}
+	return ans;
+}
+
+int findCeil(vector<int> &arr, int n, int x) {
+	int low = 0, high = n - 1;
+	int ans = -1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		if (arr[mid] >= x) {
+			ans = arr[mid];
+			high = mid - 1;
+		}
+		else {
+			low = mid + 1; 
+		}
+	}
+	return ans;
+}
+
+pair<int, int> getFloorAndCeil(vector<int> &arr, int n, int x) {
+	int f = findFloor(arr, n, x);
+	int c = findCeil(arr, n, x);
+	return make_pair(f, c);
+}
